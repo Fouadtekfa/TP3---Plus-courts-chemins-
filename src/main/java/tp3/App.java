3709e71c7c87fe4999acfef3dcb3748e0ea98ce9
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2022-11-07 16:29:20 +0100

    ajout des bibliothèques et test  Random graph generator

--- src/main/java/tp3/App.java
+++ src/main/java/tp3/App.java
@@ -1,5 +1,10 @@
 package tp3;
 
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.setProperty("org.graphstream.ui", "swing");
+
+        Graph graph = new SingleGraph("Random");
+        Generator gen = new RandomGenerator(2);
+        gen.addSink(graph);
+        gen.begin();
+        for(int i=0; i<100; i++)
+            gen.nextEvents();
+        gen.end();
+        graph.display();
     }
 }
