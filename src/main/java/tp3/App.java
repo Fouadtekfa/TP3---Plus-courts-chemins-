ab4153709ca90e94bac67198d2704e921907170b
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2022-11-16 18:14:34 +0100

    ajout de la  fontion GeneratorGraph, geration de plusieurs graphes et test

--- src/main/java/tp3/App.java
+++ src/main/java/tp3/App.java
@@ -5,23 +5,33 @@ import org.graphstream.algorithm.generator.RandomGenerator;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+
+
+public class App {
+
+
+    private static void RandomGenerator(Graph graph, int NbNoueds , double averageDegree) {
+
+        RandomGenerator generator =new RandomGenerator(averageDegree);
+        generator.addSink(graph);
+
+        generator.addSink(graph);
+        generator.begin();
+        for(int i=0; i<NbNoueds; i++)
+            generator.nextEvents();
+        generator.end();
+        graph.display();
+
+    }
     public static void main( String[] args )
     {
-        System.setProperty("org.graphstream.ui", "swing");
 
-        Graph graph = new SingleGraph("Random");
-        Generator gen = new RandomGenerator(2);
-        gen.addSink(graph);
-        gen.begin();
-        for(int i=0; i<100; i++)
-            gen.nextEvents();
-        gen.end();
-        graph.display();
+        System.setProperty("org.graphstream.ui", "swing");
+        Graph graph1 = new SingleGraph("Random");
+        RandomGenerator(graph1,100,3);
+        Graph graph2 = new SingleGraph("Random");
+        RandomGenerator(graph2,150,2);
+        Graph graph3 = new SingleGraph("Random");
+        RandomGenerator(graph3,400,3);
     }
 }
